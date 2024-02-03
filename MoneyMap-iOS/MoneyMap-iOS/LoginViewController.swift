3357d00c4eb7fc92f6d7857130bfced865c030d4
venugopalreddy701 <venugopalreddy701@gmail.com>
2024-02-03 15:50:33 +0530

    Project Setup:No storyboard

---
+++ MoneyMap-iOS/MoneyMap-iOS/LoginViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  MoneyMap-iOS
+//
+//  Created by Venugopal Reddy M on 03/02/24.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        title = "Home"
+        view.backgroundColor =  .green
+    }
+
+
+}
+
