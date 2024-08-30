5cf50db15e859585b4d81a197ebb644a920a581a
Venugopal Reddy <venugopalreddy701@gmail.com>
2024-08-30 10:21:54 +0530

    API response file updated

---
+++ MoneyMap_iOS/Services/APIResponse/UserCreationResponse.swift
@@ -0,0 +1,13 @@
+//
+//  UserCreationResponse.swift
+//  MoneyMap_iOS
+//
+//  Created by Venugopal on 26/08/24.
+//
+
+import Foundation
+struct UserCreationResponse:Decodable{
+    let id:Int
+    let email:String
+    let profile:String
+}
